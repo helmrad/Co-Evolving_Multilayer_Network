da8cc5acab92ea7c9187d3badc4886e6b33daab4
ga57sig <conrad.helminger@tum.de>
2020-05-04 14:54:14 +0200

    Readme & Code Update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Co-Evolving Multilayer Network
+
+This code simulates a co-evolving multilayer network, that is, a network with multiple types of interactions between the nodes, while both the state of the nodes and the links that connect the nodes evolve dynamically.
+
+![alt text](https://github.com/helmrad/Co-evolving_Multilayer_Network/images/simulation_screenshot.svg "Screenshot of the network simulation")
+
+Created with Python 3.7
+Dependencies:
+- matplotlib 3.2.1
+- numpy 1.18.4
+- networkx 2.4
+- names 0.3.0
--- co-evolving_multilayer_network.py
+++ co-evolving_multilayer_network.py
@@ -1,28 +1,15 @@
 # -*- coding: utf-8 -*-
-# Created with Python 3.6
-"""
-This code generates a co-evolving network.
-An exchange of a quantity takes place on the network, while the weights of the links change randomly.
-"""
+# Created with Python 3.7
 
-# TODO
-# implement multilayer dynamics
-# try colored interlayer-links
-# support different layouts
-# make dynamic view larger
-
-# TODO Project
-# Assign names of banks (maybe automated like bank of <random European country>)
-# Put a map of Europe below and use markers to affiliate nodes with countries
-# Three layers as given by Thurner
-# Graphs on the right, like total quantity over time and stock indices
-# Dynamics
+"""
+This code generates a co-evolving multilayer network.
+The state of the network (some quantity that is exchanged) and the weights of the links change randomly.
+"""
 
 import pylab
 import matplotlib.pyplot as plt
 from matplotlib.patches import ArrowStyle
 import numpy as np
-from numpy import matlib as ml
 import networkx as nx
 import names
 import copy
@@ -33,6 +20,7 @@ import random
 class CoevolvingMultilayerNetwork:
 
     def __init__(self, layers, nodes, link_p):
+        # Initialize parameters
         # Network parameters
         self.layers = layers
         self.nodes = nodes
@@ -49,10 +37,15 @@ class CoevolvingMultilayerNetwork:
         self.link_size_scaling = 10
         self.nodesets = layers + 1
         self.layer_coords = [[0, layer] for layer in range(0, self.layers)]
-        self.layer_coords.append([1, self.layers-1])
+        self.layer_coords.append([1, self.layers-1])  # coordinates of the dynamic node view
         self.dynamic_node_range = range(self.layers*self.nodes, (self.layers+1)*self.nodes)
         self.colors = ['#003071', 'white', 'grey']
         self.arrow = ArrowStyle('simple', head_length=25, head_width=20, tail_width=.75)
+        self.layer_x_dist = 10
+        self.layer_y_dist = 10
+        self.layer_scale = 3
+        self.dynamic_node_scale = 3
+        self.txt_fontsize = 15
 
         # Simulation parameters
         self.iterations = 1000
@@ -71,7 +64,7 @@ class CoevolvingMultilayerNetwork:
             self.funcs.append(func1) if np.random.uniform() > 0.5 else self.funcs.append(func2)
 
     def initialize_links(self):
-        # Get adjacency tensor
+        # Compute an adjacency tensor for multilayer connectivity
         links = np.zeros((self.nodesets, self.nodes, self.nodes))
         for layer in range(0, self.layers):
             layer_links = np.random.uniform(self.link_w_base-self.link_w_dev, self.link_w_base+self.link_w_dev,
@@ -84,7 +77,7 @@ class CoevolvingMultilayerNetwork:
         return links
 
     def initialize_labels_and_quantities(self):
-        # Get names & quantities of nodes
+        # Get random labels & quantities of nodes
         name_list, quantities = [], []
         for i in range(self.nodes):
             name = names.get_first_name()
@@ -97,20 +90,18 @@ class CoevolvingMultilayerNetwork:
         return labels, quantities
 
     def initialize_layout(self):
-        # Get layouting parameters
-        auxiliary_layers = [(nx.Graph(), self.layer_coords[i]) for i in range(0, self.nodesets)]
-        # Nodes per layer
-        ranges = [(self.nodes*i, self.nodes*(i+1)) for i in range(0, self.nodesets)]
-        # Fill the auxiliary layers with nodes from the network
-        for (aux_layer, coord), (start, end) in zip(auxiliary_layers, ranges):
-            aux_layer.add_nodes_from(itertools.islice(self.net.nodes, start, end))
-        # Calculate and move the nodes position
+        # Calculate the positions of all nodes in the network visualization
+        # Generate an auxiliary graph and fill it with nodes from the network
+        auxiliary_graph = nx.Graph()
+        auxiliary_graph.add_nodes_from(itertools.islice(self.net.nodes, 0, self.nodes))
+        # Set the layout for one layer, and offset the positions of nodes in each other layer accordingly
         all_pos = {}
-        for aux_layer, (sx, sy) in auxiliary_layers:
-            pos = nx.shell_layout(aux_layer, scale=2)  # or spring_layout...
+        pos = nx.shell_layout(auxiliary_graph)
+        for l, (sx, sy) in zip(range(0, self.nodesets), self.layer_coords):
             for node in pos:
-                all_pos[node] = pos[node]
-                all_pos[node] += (10 * sx, 10 * sy)
+                all_pos[l*self.nodes+node] = pos[node]*self.layer_scale if l != self.layers \
+                                             else pos[node]*self.dynamic_node_scale  # scale dynamic view larger
+                all_pos[l*self.nodes+node] += (self.layer_x_dist*sx, self.layer_y_dist*sy)
         return all_pos
 
     def initialize_colormap(self):
@@ -137,21 +128,23 @@ class CoevolvingMultilayerNetwork:
         return links_mat
 
     def links_matrix_to_tensor(self, links_mat):
+        # Convert networkx-compliant matrix form to an adjancecy tensor
         links = np.zeros((self.nodesets, self.nodes, self.nodes))
         for n in range(0, self.layers):
             links[n, :, :] = links_mat[self.nodes*n:self.nodes*(n+1), self.nodes*n:self.nodes*(n+1)]
         return links
 
-    def links_matrix_to_array(self):
-        # Flatten links matrix
+    def links_matrix_to_array(self, links_mat):
+        # Convert a matrix to a 1d list
         links_flat = []
-        for lin in range(len(self.links)):
-            for col in range(len(self.links)):
-                if self.links[lin, col] != 0:
-                    links_flat.append(self.links[lin, col])
+        for lin in range(len(links_mat)):
+            for col in range(len(links_mat)):
+                if links_mat[lin, col] != 0:
+                    links_flat.append(links_mat[lin, col])
         return links_flat
 
     def initialize(self):
+        # A second initialization (next to __init__) for all parameters and variables that are more complex
         self.links = self.initialize_links()
         links_mat = self.links_tensor_to_matrix()
         # links = self.links_matrix_to_tensor(links_mat)
@@ -163,6 +156,7 @@ class CoevolvingMultilayerNetwork:
         self.assign_quantities()
 
     def update_visualization(self):
+        # Calculate sizes of links and nodes, and update the visualization of the network
         nodesizes = copy.deepcopy(self.quantities)
         for n in range(len(nodesizes)):
             nodesizes[n] = self.node_size_min if nodesizes[n] < self.node_size_min else nodesizes[n]
@@ -181,30 +175,26 @@ class CoevolvingMultilayerNetwork:
         nx.draw_networkx_edges(self.net, pos=self.all_pos, edgelist=self.links_interlayer,
                                style='dotted', edge_color=self.colors[2], alpha=.5, arrows=False)
 
-        txt = 'Total Quantity ' + str(np.round(np.sum(self.quantities), 2))
-        #plt.text(1, 1, txt, horizontalalignment='center', verticalalignment='center', fontsize=20)
-        #pylab.draw()
+        # Animate the entire amount of quantity in the network
+        txt = 'Entire quantity ' + str(np.round(np.sum(self.quantities), 2))
+        plt.text(self.layer_x_dist, 0, txt, horizontalalignment='center',
+                 verticalalignment='center', fontsize=self.txt_fontsize)
         plt.pause(.001)
         plt.clf()
 
     def network_dynamics(self):
-        # Quantities
-        # Amount of quantity transferred to each node
-        # transfers = np.dot(quantities, links)
-        # # Amount of quantity lost at each node
-        # quantities_rep = ml.repmat(quantities, len(quantities), 1)
-        # losses = np.diagonal(np.dot(links, quantities_rep))
-        # # Merge losses & transfers
-        # #quantities = [quantities[n] - losses[n] + funcs[n](transfers[n]) for n in range(len(name_list))]
-        # quantities = [quantities[n] - losses[n] + transfers[n] for n in range(NODESETS*NODES)]
+        # Co-evolving dynamics
+        # Restore links in tensor form
+        self.links = self.links_matrix_to_tensor(nx.convert_matrix.to_numpy_matrix(self.net))
+        # Random quantitiy dynamics
         self.quantities = [quantity + np.random.uniform(-1, 1)*0.05 for quantity in self.quantities]
-        # Links: restore & alter
-        self.links = np.array(nx.convert_matrix.to_numpy_matrix(self.net))
+        # Random link dynamics
         self.links = np.multiply(self.links, np.random.uniform(self.link_mod_base-self.link_mod_dev,
                                                                self.link_mod_base+self.link_mod_dev,
                                                                size=self.links.shape))
 
     def assign_quantities(self):
+        # Assign quantities to nodes
         quantities_dict = dict(zip(self.dynamic_node_range, self.quantities))
         nx.set_node_attributes(self.net, quantities_dict, 'Quantities')
 
@@ -222,7 +212,8 @@ class CoevolvingMultilayerNetwork:
         return links_flat
 
     def constrain_and_reassign(self):
-        links_flat = self.links_matrix_to_array()
+        # Constrain both links and quantities, and reassign to the network
+        links_flat = self.links_matrix_to_array(self.links_tensor_to_matrix())
         links_flat = self.limit_params(links_flat)
         # Reassign quantities to nodes and weights to links
         self.assign_quantities()
@@ -230,6 +221,7 @@ class CoevolvingMultilayerNetwork:
             edge_triple[-1]['weight'] = link
 
     def simulate(self):
+        # Set up the plot and start simulating the dynamics and updating the visualization
         pylab.ion()
         fig = plt.figure(0, figsize=(16, 8))
         fig.canvas.set_window_title('Propagation of a Quantity on a Dynamic Network')
@@ -244,6 +236,7 @@ class CoevolvingMultilayerNetwork:
 
 
 if __name__ == "__main__":
-    network = CoevolvingMultilayerNetwork(layers=3, nodes=20, link_p=.15)
+    # Set up & simulate the co-evolving multilayer network
+    network = CoevolvingMultilayerNetwork(layers=3, nodes=15, link_p=.15)
     network.initialize()
     network.simulate()
